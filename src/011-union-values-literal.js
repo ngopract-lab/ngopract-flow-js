5a1b75dc2caff22175a45fd2af4c3317d433ccc6
Adhy <adhywiranatap@gmail.com>
2017-04-11 14:10:17 +0700

    added union values literal

---
+++ src/011-union-values-literal.js
@@ -0,0 +1,13 @@
+// @flow
+function getColor(name: "success" | "warning" | "danger") {
+  switch (name) {
+    case "success" : return "green";
+    case "warning" : return "yellow";
+    case "danger"  : return "red";
+  }
+}
+
+getColor("success"); // Works!
+getColor("danger");  // Works!
+// $ExpectError
+// getColor("error"); // Error!
