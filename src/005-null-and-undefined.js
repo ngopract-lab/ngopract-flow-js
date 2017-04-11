83d9857ad7649368f39599bf095c3fa42bd6af66
Adhy <adhywiranatap@gmail.com>
2017-04-11 13:51:44 +0700

    updated null and undefined module

---
+++ src/005-null-and-undefined.js
@@ -0,0 +1,13 @@
+// @flow
+function acceptsNull(value: null) {
+  /* ... */
+}
+
+function acceptsUndefined(value: void) {
+  /* ... */
+}
+
+acceptsNull(null);      // Works!
+// acceptsNull(undefined); // Error!
+acceptsUndefined(null);      // Error!
+// acceptsUndefined(undefined); // Works!
